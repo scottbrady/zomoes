3dd2695cd2c624b697e4be4ffc4b52a69aa969e6
Scott Brady <sbrady@webassign.net>
2013-01-08 17:53:12 -0500

    Install the bundle when the VM starts.

--- chef/cookbooks/zomoes/recipes/post.rb
+++ chef/cookbooks/zomoes/recipes/post.rb
@@ -2,3 +2,10 @@ execute "clear firewall rules" do
     # if we don't clear the firewall rules we won't be able to connect
     command "iptables -F"
 end
+
+bash "install_gems_bundle" do
+    cwd "/var/www"
+    code <<-EOH
+        bundle install
+    EOH
+end

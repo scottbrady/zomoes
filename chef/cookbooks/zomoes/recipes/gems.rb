d29f40f2d658fff8f9ecd67a8abc9269690590cf
Scott Brady <sbrady@webassign.net>
2013-01-08 13:22:50 -0500

    Now installing the MongoMapper library.

--- chef/cookbooks/zomoes/recipes/gems.rb
+++ chef/cookbooks/zomoes/recipes/gems.rb
@@ -1,7 +1,5 @@
 dataBag = data_bag_item('zomoes', 'configs')
 
 dataBag['gems'].each do |gem|
-    execute "installing GEM: #{gem}" do
-        command "gem install #{gem}"
-    end
+    gem_package "#{gem}"
 end

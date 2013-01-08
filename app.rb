31de07dede0f3800275252f882e4d39c69a9b9fe
Scott Brady <sbrady@webassign.net>
2013-01-08 16:08:07 -0500

    Using bundler and the mongo mapper library.

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
-require 'find'
-require 'rubygems'
-require 'sinatra'
+require "find"
+require "rubygems"
+require "bundler/setup"
+
+require "mongo_mapper"
+require "sinatra"
+
+$: << "./lib"
+
+MongoMapper.database = "zomoes"
 
 set :port, 8080
 
 # recursively find and require all controllers
-Find.find('./lib/controllers') do |file|
-    require file if file.end_with?('.rb')
+Find.find("./lib/controllers") do |file|
+    require file if file.end_with?(".rb")
 end

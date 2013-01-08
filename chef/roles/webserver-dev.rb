fd7eaa97e1e8b9d5961054e0fad2e01ac7fa8d9f
Scott Brady <sbrady@webassign.net>
2013-01-08 11:53:55 -0500

    Installing mongo in the VM.

--- chef/roles/webserver-dev.rb
+++ chef/roles/webserver-dev.rb
@@ -1,7 +1,9 @@
 name        "webserver-dev"
 description "Development webserver"
 run_list(
-    # "recipe[zomoes::pre]",
+    "recipe[yum]",
+    "recipe[mongodb::10gen_repo]",
+    "recipe[mongodb::default]",
     "recipe[zomoes::gems]",
     "recipe[zomoes::post]"
 )
